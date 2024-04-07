9aeeac291fe86ea06da0cff0d419e467a826e444
nicapos <67004893+nicapos@users.noreply.github.com>
2024-04-07 15:52:40 +0800

    feat: add working color change

--- src/modules/color-change.js
+++ src/modules/color-change.js
@@ -1,4 +1,5 @@
-import { defaultColor } from "./constants";
+import { defaultColor, mesh } from "./constants";
+import { scene } from "../scenes/cake";
 
 // Setup default color values in form after DOM loads
 document.addEventListener('DOMContentLoaded', function() {
@@ -6,4 +7,40 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("frosting-color").value = defaultColor.frosting;
   document.getElementById("candle-color").value = defaultColor.candle;
   document.getElementById("base-color").value = defaultColor.base;
+});
+
+document.getElementById("icing-color").addEventListener('input', function(event) {
+  const selectedColor = event.target.value;
+  scene.traverse(function(child) {
+      if (child.isMesh && (child.material.name === mesh.icing.top || child.material.name === mesh.icing.bottom)) {
+          child.material.color.set(selectedColor);
+      }
+  });
+});
+
+document.getElementById("frosting-color").addEventListener('input', function(event) {
+  const selectedColor = event.target.value;
+  scene.traverse(function(child) {
+      if (child.isMesh && (child.material.name === mesh.frosting)) {
+          child.material.color.set(selectedColor);
+      }
+  });
+});
+
+document.getElementById("candle-color").addEventListener('input', function(event) {
+  const selectedColor = event.target.value;
+  scene.traverse(function(child) {
+      if (child.isMesh && (child.material.name === mesh.candle)) {
+          child.material.color.set(selectedColor);
+      }
+  });
+});
+
+document.getElementById("base-color").addEventListener('input', function(event) {
+  const selectedColor = event.target.value;
+  scene.traverse(function(child) {
+      if (child.isMesh && (child.material.name === mesh.base.top || child.material.name === mesh.base.bottom)) {
+          child.material.color.set(selectedColor);
+      }
+  });
 });
---
+++ src/modules/constants.js
@@ -0,0 +1,21 @@
+// Names of the meshes from the imported blender file
+export const mesh = {
+  icing: {
+    top: "Material.002",
+    bottom: "Material"
+  },
+  frosting: "Material.004", // TODO: Verify mesh name
+  candle: "Material.005", // TODO: Verify mesh name
+  base: {
+    top: "Material.003", // TODO: Verify mesh name
+    bottom: "Material.001", // TODO: Verify mesh name
+  },
+};
+
+// Default color of meshes from the imported blender file
+export const defaultColor = {
+  icing: "#E700D5",
+  frosting: "#E8106E",
+  candle: "#0074E7",
+  base: "#411900",
+}
