a4262e899c6de24c8431d329c3ed8f317b4ddfe1
nicapos <67004893+nicapos@users.noreply.github.com>
2024-04-07 15:43:53 +0800

    refactor: move three.js to container

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { scene, camera} from './scenes/cake';
 
+// Select the container element in HTML
+const container = document.getElementById('threejs-container');
+
 // Create renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xfaede3);
-document.body.appendChild(renderer.domElement);
+container.appendChild(renderer.domElement);
+
+// Handle window resize
+function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+}
+window.addEventListener('resize', onWindowResize);
 
 // Add orbit controls to make scene draggable/controllable
 const controls = new OrbitControls(camera, renderer.domElement);
